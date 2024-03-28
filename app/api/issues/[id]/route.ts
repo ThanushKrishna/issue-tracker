9b17a34cc28bc14adbc7b7247f6977c266ca09f5
ThanushKrishna <jeevanthanush@gmail.com>
2024-03-28 23:05:57 +0530

    Enhance the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -2,7 +2,7 @@ import authOptions from '@/app/auth/authOptions';
 import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../../prisma/client";
-import { createIssueSchema } from "../../../validationSchema";
+import { patchIssueSchema } from "../../../validationSchema";
 
 export async function PATCH(
   request: NextRequest, 
@@ -12,9 +12,22 @@ export async function PATCH(
   if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
-  const validation = createIssueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+    const { assignedToUserId, title, description } = body;
+
+    if (assignedToUserId) {
+      const user = await prisma.user.findUnique({
+        where: { id: assignedToUserId },
+      });
+      if (!user)
+        return NextResponse.json(
+          { error: "Invalid user." },
+          { status: 400 }
+        );
+    }
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) }
@@ -26,8 +39,9 @@ export async function PATCH(
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description
+      title,
+      description,
+      assignedToUserId
     }
   });
 
